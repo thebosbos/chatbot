78f778faf86b88c60be0114b60d148d6f80d423d
thebosbos <83311147+thebosbos@users.noreply.github.com>
2024-05-06 13:20:34 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,97 @@
+from flask import Flask, request
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, pipeline
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    Trainer,
+    HfArgumentParser,
+    TrainingArguments,
+    pipeline,
+    logging,
+)
+import transformers
+
+################################################################################
+# bitsandbytes parameters
+################################################################################
+
+# Activate 4-bit precision base model loading
+use_4bit = True
+
+# Compute dtype for 4-bit base models
+bnb_4bit_compute_dtype = "float16"
+
+# Quantization type (fp4 or nf4)
+bnb_4bit_quant_type = "nf4"
+
+# Activate nested quantization for 4-bit base models (double quantization)
+use_nested_quant = False
+# Load the entire model on the GPU 0
+device_map = {"": 0}
+model_name="bosbos/llama3finetune"
+# Load tokenizer and model with QLoRA configuration
+compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=use_4bit,
+    bnb_4bit_quant_type=bnb_4bit_quant_type,
+    bnb_4bit_compute_dtype=compute_dtype,
+    bnb_4bit_use_double_quant=use_nested_quant,
+)
+# Load base model
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    quantization_config=bnb_config,
+    device_map=device_map
+)
+model.config.use_cache = False
+model.config.pretraining_tp = 1
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    model_kwargs={"torch_dtype": torch.bfloat16},
+
+)
+
+app = Flask(__name__)
+
+@app.route('/get_response')
+def chat_function():
+    message = request.args.get('message')
+    history = request.args.get('history')
+    system_prompt = request.args.get('system_prompt')
+    max_new_tokens = int(request.args.get('max_new_tokens'))
+    temperature = float(request.args.get('temperature'))
+
+    messages = [{"role":"system","content":system_prompt},
+                {"role":"user", "content":message}]
+    prompt = pipeline.tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True,)
+    terminators = [
+        pipeline.tokenizer.eos_token_id,
+        pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")]
+    outputs = pipeline(
+        prompt,
+        max_new_tokens = max_new_tokens,
+        eos_token_id = terminators,
+        do_sample = True,
+        temperature = temperature + 0.1,
+        top_p = 0.9,)
+    return outputs[0]["generated_text"][len(prompt):]
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
